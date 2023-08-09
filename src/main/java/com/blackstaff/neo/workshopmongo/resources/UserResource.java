f15d4aae07724ff4dbebb511153afe723b63b1cb
Ralph Rabelo <ralphrabelo@gmail.com>
2023-08-09 20:19:10 -0300

    Obtendo um post por id

--- src/main/java/com/blackstaff/neo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/blackstaff/neo/workshopmongo/resources/UserResource.java
@@ -68,5 +68,4 @@ public class UserResource {
 		User obj =  service.findById(id);
 		return ResponseEntity.ok().body(obj.getPosts());
 	}
-	
 }
---
+++ src/main/java/com/blackstaff/neo/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.blackstaff.neo.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.blackstaff.neo.workshopmongo.domain.Post;
+import com.blackstaff.neo.workshopmongo.repositories.PostRepository;
+import com.blackstaff.neo.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
