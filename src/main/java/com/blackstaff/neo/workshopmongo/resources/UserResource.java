0777a38413efa9daca336c386b7804e8a5c37c9a
Ralph Rabelo <ralphrabelo@gmail.com>
2023-08-09 10:19:00 -0300

    Entity User e REST funcionando

---
+++ src/main/java/com/blackstaff/neo/workshopmongo/resources/UserResource.java
@@ -0,0 +1,28 @@
+package com.blackstaff.neo.workshopmongo.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.blackstaff.neo.workshopmongo.domain.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+
+	@GetMapping
+	public List<User> findAll() {
+		User maria = new User("1", "Maria Brown", "maria@gmail.com");
+		User alex = new User("2", "Alex", "alex@gmail.com");
+		List<User> list =  new ArrayList<>();
+		list.addAll(Arrays.asList(maria, alex));
+		return list;
+	}
+	
+	
+		
+}
