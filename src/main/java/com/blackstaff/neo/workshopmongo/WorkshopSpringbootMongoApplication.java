b5dcb87826e35005d43060f4e341c1ce7864a32d
Ralph Rabelo <ralphrabelo@gmail.com>
2023-08-08 21:08:45 -0300

    initial commit

---
+++ src/main/java/com/blackstaff/neo/workshopmongo/WorkshopSpringbootMongoApplication.java
@@ -0,0 +1,13 @@
+package com.blackstaff.neo.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringbootMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringbootMongoApplication.class, args);
+	}
+
+}
